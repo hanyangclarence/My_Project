e26af511c041a4f04c809c40e3f9a15a2153b6d9
Cralence <mahlerrrr76@gmail.com>
2024-01-30 00:09:33 +0800

    update

--- tempt.py
+++ tempt.py
@@ -16,7 +16,7 @@ pkg = torch.load(path, map_location='cpu')
 cfg = OmegaConf.create(pkg['xp.cfg'])
 model = get_compression_model(cfg)
 model.load_state_dict(pkg['best_state'])
-model = model.cuda()
+model = model.cpu()
 os.makedirs('stat', exist_ok=True)
 os.makedirs('stat/wav', exist_ok=True)
 
@@ -25,7 +25,7 @@ with open('stat/rank_recon_loss.txt', 'w') as f:
     for i, file in enumerate(file_list):
         waveform, sr = librosa.load(os.path.join('data/music/audios', file), sr=32000)
 
-        waveform = torch.tensor(waveform)[None, None, ...].cuda()
+        waveform = torch.tensor(waveform)[None, None, ...]
         waveform = waveform[..., :32000 * 10]
         recon = model.forward(waveform).x
 
@@ -35,9 +35,9 @@ with open('stat/rank_recon_loss.txt', 'w') as f:
         print(f'{i+1} {file}: loss: {recon_loss}')
         soundfile.write(os.path.join('stat/wav', file), waveform.squeeze().detach().cpu(), samplerate=32000)
 
-        waveform = waveform.cpu()
-        del waveform
-        torch.cuda.empty_cache()
+        # waveform = waveform.cpu()
+        # del waveform
+        # torch.cuda.empty_cache()
 
     sorted_result = dict(sorted(results.items(), key=lambda x: x[1]))
     for k, v in sorted_result.items():
