8328cbe82a28dd8e4845fa332f5feafa66f7a880
Cralence <mahlerrrr76@gmail.com>
2024-02-01 14:47:01 +0800

    update

--- tempt.py
+++ tempt.py
@@ -1,40 +1,27 @@
 import os
-import librosa
+import json
 import matplotlib.pyplot as plt
-from omegaconf import OmegaConf
 import torch
-from unimumo.audio.audiocraft_.models.builders import get_compression_model
 
+data_dir = 'data/music/music4all_beat'
+file_list = os.listdir(data_dir)[:1000]
 
-file_list = os.listdir('../Data_analysis/audios')
-path = '../My_Tempt_Repo/pretrained/music_vqvae.bin'
-pkg = torch.load(path, map_location='cpu')
-cfg = OmegaConf.create(pkg['xp.cfg'])
-model = get_compression_model(cfg)
-model.load_state_dict(pkg['best_state'])
-model = model.cuda()
+with open('rank_beat_std.json', 'w') as f:
+    results = {}
+    for i, file in enumerate(file_list):
+        data = torch.load(os.path.join(data_dir, file))
+        beat = data['beat']
+        interval = beat[1:] - beat[:-1]
+        beat_std = interval.std()
 
-with open('rank_recon_loss.txt', 'w') as f:
-    with torch.no_grad():
-        results = {}
-        for i, file in enumerate(file_list):
-            waveform, sr = librosa.load(os.path.join('../Data_analysis/audios', file), sr=32000)
+        results[file] = beat_std
+        print(f'{i+1} {file}: std: {beat_std}')
 
-            waveform = torch.tensor(waveform)[None, None, ...].cuda()
-            # waveform = waveform[..., :32000 * 6]
-            recon = model.forward(waveform).x
+    sorted_result = dict(sorted(results.items(), key=lambda x: x[1]))
+    json.dump(sorted_result, f, indent=4)
 
-            recon_loss = torch.nn.functional.mse_loss(waveform, recon).item()
-
-            results[file] = recon_loss
-            print(f'{i+1} {file}: loss: {recon_loss}')
-
-        sorted_result = dict(sorted(results.items(), key=lambda x: x[1]))
-        for k, v in sorted_result.items():
-            f.write(f'{k}\t{v}\n')
-
-        values = sorted_result.values()
-        plt.hist(values, bins=80)
-        plt.savefig('recon_loss_histogram.png')
+    values = sorted_result.values()
+    plt.hist(values, bins=80)
+    plt.savefig('beat_std_histogram.png')
 
 # 0.0025 < loss   (to be further decided)
