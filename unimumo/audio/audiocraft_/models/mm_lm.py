0305fd6468cc3bf358d21e85ff56b7a64a955e64
Cralence <mahlerrrr76@gmail.com>
2024-02-05 01:47:28 +0800

    comment two lines for debug!

--- unimumo/audio/audiocraft_/models/mm_lm.py
+++ unimumo/audio/audiocraft_/models/mm_lm.py
@@ -317,8 +317,8 @@ class LMModel(StreamingModule):
         mask = torch.zeros((section_1 + section_2, section_1 + section_2), dtype=torch.bool, device=device)
 
         mask[:section_1, :section_1] = ~torch.ones((section_1, section_1), dtype=torch.bool, device=device).triu(1)
-        mask[section_1:section_1 + section_2, :section_1] = ~torch.ones((section_2, section_1), dtype=torch.bool, device=device).triu(1)
-        mask[:section_1, section_1:section_1 + section_2] = ~torch.ones((section_1, section_2), dtype=torch.bool, device=device).triu(1)
+        # mask[section_1:section_1 + section_2, :section_1] = ~torch.ones((section_2, section_1), dtype=torch.bool, device=device).triu(1)
+        # mask[:section_1, section_1:section_1 + section_2] = ~torch.ones((section_1, section_2), dtype=torch.bool, device=device).triu(1)
         mask[section_1:section_1 + section_2, section_1:section_1 + section_2] = ~torch.ones((section_2, section_2), dtype=torch.bool, device=device).triu(1)
 
         # if mode == 'music2motion':
