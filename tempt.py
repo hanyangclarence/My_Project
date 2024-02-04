be15822e5e14bac22336e6c422d25fb6d3dc009c
Cralence <mahlerrrr76@gmail.com>
2024-02-04 13:51:21 +0800

    update

--- tempt.py
+++ tempt.py
@@ -1,51 +1,18 @@
-import os
-import librosa
-import matplotlib.pyplot as plt
-from omegaconf import OmegaConf
 import torch
-from unimumo.audio.audiocraft_.models.builders import get_compression_model
-import random
-import soundfile
-from pytorch_lightning import seed_everything
 
-seed_everything(42)
-file_list = os.listdir('data/music/audios')
-file_list = random.sample(file_list, 1000)
-path = 'pretrained/music_vqvae.bin'
-pkg = torch.load(path, map_location='cpu')
-cfg = OmegaConf.create(pkg['xp.cfg'])
-model = get_compression_model(cfg)
-model.load_state_dict(pkg['best_state'])
-model = model.cuda()
-os.makedirs('stat', exist_ok=True)
-os.makedirs('stat/wav', exist_ok=True)
+path_og = '../My_Tempt_Repo/pretrained/musicgen_small.bin'
+path_trained = 'training_logs/2024-02-02T09-28-27_exp_3_2_stage1/checkpoints/e_18.ckpt'
 
-with open('stat/rank_recon_loss.txt', 'w') as f:
-    with torch.no_grad():
-        results = {}
-        for i, file in enumerate(file_list):
-            waveform, sr = librosa.load(os.path.join('data/music/audios', file), sr=32000)
+og_ckpt = torch.load(path_og, map_location='cpu')
+new_ckpt = torch.load(path_trained, map_location='cpu')
 
-            waveform = waveform.reshape(-1, waveform.shape[-1] // 5)
-            waveform = torch.tensor(waveform).unsqueeze(1).cuda()
-            recon = model.forward(waveform).x
-            print(f'wave: {waveform.shape}, recon: {recon.shape}')
+og_ckpt = og_ckpt['best_state']
+new_ckpt = new_ckpt['state_dict']
 
-            recon_loss = torch.nn.functional.mse_loss(waveform, recon)
-
-            results[file] = recon_loss
-            print(f'{i+1} {file}: loss: {recon_loss}')
-            soundfile.write(os.path.join('stat/wav', file), waveform.squeeze().detach().cpu().numpy().reshape(-1), samplerate=32000)
-
-            waveform = waveform.cpu()
-            del waveform
-            torch.cuda.empty_cache()
-
-        sorted_result = dict(sorted(results.items(), key=lambda x: x[1]))
-        for k, v in sorted_result.items():
-            f.write(f'{k}\t{v}\n')
-
-        values = sorted_result.values()
-        plt.hist(values, bins=80)
-        plt.savefig('stat/recon_loss_histogram.png')
+for name, param in og_ckpt.items():
+    new_name = 'model.' + name
+    if new_name in new_ckpt.keys():
+        print(f"{new_name}, {torch.sum(param - new_ckpt[new_name])}")
+    else:
+        print(f'{name}, {new_name}')
 
