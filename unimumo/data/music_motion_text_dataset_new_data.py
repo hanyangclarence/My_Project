b2a05d2aaa9b9a9340c17056d62bda4d81906434
Cralence <mahlerrrr76@gmail.com>
2024-02-04 10:23:48 +0800

    update

--- unimumo/data/music_motion_text_dataset_new_data.py
+++ unimumo/data/music_motion_text_dataset_new_data.py
@@ -9,9 +9,6 @@ import pandas as pd
 import json
 
 
-# Experiment
-# Also load the gpt generated text
-
 class MusicMotionTextDataset(Dataset):
     def __init__(
         self, split, music_meta_dir, motion_meta_dir,
@@ -19,8 +16,8 @@ class MusicMotionTextDataset(Dataset):
         duration=10,
         vqvae_sr=32000,
         dropout_prob=0.1,
-        music_dataset_name='music4all',
-        ignore_file_name='music4all_ignore.txt',
+        music_dataset_name='spotify',
+        ignore_file_name='spotify_ignore.txt',
         natural_language_caption_ratio=0.3,
         train_caption=False
     ):
@@ -36,7 +33,6 @@ class MusicMotionTextDataset(Dataset):
         self.vqvae_sr = vqvae_sr
         self.music_target_length = int(duration * 50)
         self.dropout_prob = dropout_prob
-        self.natural_language_caption_ratio = natural_language_caption_ratio
         self.train_caption = train_caption
 
         # all data lists
@@ -50,19 +46,8 @@ class MusicMotionTextDataset(Dataset):
 
         # load data related to text descriptions
         # load metadata of music4all
-        self.text_df = pd.read_csv(pjoin(self.music_meta_dir, 'music4all_metadata.csv'), index_col=0)
-        # load mu-llama generated text descriptions
-        with open(pjoin(self.music_meta_dir, 'music4all_captions_mullama.json'), 'r') as caption_fd:
-            llama_music_caption = json.load(caption_fd)
-        # load gpt generated text descriptions
-        with open(pjoin(self.music_meta_dir, 'music4all_captions_gpt.json'), 'r') as caption_fd:
-            self.music_caption = json.load(caption_fd)
-        # merge two descriptions
-        for k, v in llama_music_caption.items():
-            if k in self.music_caption.keys():
-                if 'male vocalist' in v:
-                    continue
-                self.music_caption[k].append(v)
+        self.text_df = pd.read_csv(pjoin(self.music_meta_dir, 'music_2_new.csv'), index_col=0)
+
         # load humanml3d text descriptions
         humanml3d_text_dir = pjoin(self.motion_meta_dir, 'humanml3d_text_description')
         humanml3d_descriptions = os.listdir(humanml3d_text_dir)
@@ -124,8 +109,6 @@ class MusicMotionTextDataset(Dataset):
                     continue
                 if not os.path.exists(pjoin(self.music_code_dir, line.strip() + '.pth')):
                     continue
-                if line.strip() not in self.music_caption.keys():
-                    continue
                 if line.strip() not in music_with_paired_motion:
                     continue
                 self.music_data.append(line.strip())
@@ -153,104 +136,81 @@ class MusicMotionTextDataset(Dataset):
         motion_code = motion_code[:, start_idx:end_idx]
 
         # music text prompt construction
-        if random.uniform(0, 1) < self.natural_language_caption_ratio:
-            # use llama caption
-            music_text_prompt = random.choice(self.music_caption[music_id])
+        # use constructed text prompt
+        music_id = music_id.split('_')[0]
+        genre = self.text_df.loc[music_id, 'genre']
+
+        # choose for tempo descriptor
+        tempo = self.text_df.loc[music_id, 'tempo']
+        if tempo < 60:
+            s1 = ['extremely', 'very']
+            s2 = ['slow', 'languid', 'lethargic', 'relaxed', 'leisure', 'chilled']
+            tempo_description = f'{random.choice(s1)} {random.choice(s2)}'
+        elif 60 <= tempo < 75:
+            tempo_description = random.choice(['slow', 'languid', 'lethargic', 'relaxed', 'leisure', 'chilled'])
+        elif 75 <= tempo < 110:
+            tempo_description = random.choice(['moderate', 'easy-going', 'laid-back', 'medium', 'balanced', 'neutral'])
+        elif 110 <= tempo < 150:
+            tempo_description = random.choice(['fast', 'upbeat', 'high', 'brisk', 'quick', 'rapid', 'swift'])
         else:
-            # use constructed text prompt
-            tag_list = self.text_df.loc[music_id, 'tags']
-            if pd.isna(tag_list):
-                tag_list = ['nan value']
-                tag_is_empty = True
-            else:
-                tag_list = tag_list.split('\t')
-                tag_is_empty = False
+            s1 = ['extremely', 'very', 'highly']
+            s2 = ['fast', 'upbeat', 'high', 'brisk', 'quick', 'rapid', 'swift']
+            tempo_description = f'{random.choice(s1)} {random.choice(s2)}'
 
-            # choose for tempo descriptor
-            tempo = self.text_df.loc[music_id, 'tempo']
-            if tempo < 60:
-                s1 = ['extremely', 'very']
-                s2 = ['slow', 'languid', 'lethargic', 'relaxed', 'leisure', 'chilled']
-                tempo_description = f'{random.choice(s1)} {random.choice(s2)}'
-            elif 60 <= tempo < 75:
-                tempo_description = random.choice(['slow', 'languid', 'lethargic', 'relaxed', 'leisure', 'chilled'])
-            elif 75 <= tempo < 110:
-                tempo_description = random.choice(['moderate', 'easy-going', 'laid-back', 'medium', 'balanced', 'neutral'])
-            elif 110 <= tempo < 150:
-                tempo_description = random.choice(['fast', 'upbeat', 'high', 'brisk', 'quick', 'rapid', 'swift'])
-            else:
-                s1 = ['extremely', 'very', 'highly']
-                s2 = ['fast', 'upbeat', 'high', 'brisk', 'quick', 'rapid', 'swift']
-                tempo_description = f'{random.choice(s1)} {random.choice(s2)}'
+        # choose for energy descriptor
+        energy = self.text_df.loc[music_id, 'energy']
+        if energy < 0.1:
+            s1 = ['extremely', 'very']
+            s2 = ['soft', 'calm', 'peaceful', 'serene', 'gentle', 'light', 'tranquil', 'mild', 'mellow']
+            energy_description = f'{random.choice(s1)} {random.choice(s2)}'
+        elif 0.1 <= energy < 0.4:
+            energy_description = random.choice(
+                ['soft', 'calm', 'peaceful', 'serene', 'gentle', 'light', 'tranquil', 'mild', 'mellow'])
+        elif 0.4 <= energy < 0.7:
+            energy_description = random.choice(['moderate', 'comfortable', 'balanced', 'relaxing'])
+        elif 0.7 <= energy < 0.95:
+            energy_description = random.choice(
+                ['intense', 'powerful', 'strong', 'vigorous', 'fierce', 'potent', 'energetic'])
+        else:
+            s1 = ['extremely', 'very', 'highly']
+            s2 = ['intense', 'powerful', 'strong', 'vigorous', 'fierce', 'potent', 'energetic']
+            energy_description = f'{random.choice(s1)} {random.choice(s2)}'
 
-            # choose for energy descriptor
-            energy = self.text_df.loc[music_id, 'energy']
-            if energy < 0.1:
-                s1 = ['extremely', 'very']
-                s2 = ['soft', 'calm', 'peaceful', 'serene', 'gentle', 'light', 'tranquil', 'mild', 'mellow']
-                energy_description = f'{random.choice(s1)} {random.choice(s2)}'
-            elif 0.1 <= energy < 0.4:
-                energy_description = random.choice(
-                    ['soft', 'calm', 'peaceful', 'serene', 'gentle', 'light', 'tranquil', 'mild', 'mellow'])
-            elif 0.4 <= energy < 0.7:
-                energy_description = random.choice(['moderate', 'comfortable', 'balanced', 'relaxing'])
-            elif 0.7 <= energy < 0.95:
-                energy_description = random.choice(
-                    ['intense', 'powerful', 'strong', 'vigorous', 'fierce', 'potent', 'energetic'])
-            else:
-                s1 = ['extremely', 'very', 'highly']
-                s2 = ['intense', 'powerful', 'strong', 'vigorous', 'fierce', 'potent', 'energetic']
-                energy_description = f'{random.choice(s1)} {random.choice(s2)}'
+        # construct phrases
+        noun_choices = ['tempo', 'speed', 'pace', 'BPM', 'rhythm', 'beat']
+        tempo_choices = [f'with a {tempo_description} {random.choice(noun_choices)}',
+                         f'whose {random.choice(noun_choices)} is {tempo_description}',
+                         f'a {tempo_description} music', f'set in a {tempo_description} {random.choice(noun_choices)}']
 
-            # drop out some tags
-            p = random.uniform(0, 1)
-            if p < self.dropout_prob:
-                tempo_description = None
-            p = random.uniform(0, 1)
-            if p < self.dropout_prob:
-                energy_description = None
-            filtered_tag_list = []
-            for tag in tag_list:
-                p = random.uniform(0, 1)
-                if p > self.dropout_prob:
-                    filtered_tag_list.append(tag)
+        noun_choices = ['intensity', 'energy']
+        energy_choices = [f'which is {energy_description}', f'with {energy_description} {random.choice(noun_choices)}',
+                          f'a {energy_description} music',
+                          f'whose {random.choice(noun_choices)} is {energy_description}']
 
-            # construct phrases
-            noun_choices = ['tempo', 'speed', 'pace', 'BPM', 'rhythm', 'beat']
-            tempo_choices = [f'with a {tempo_description} {random.choice(noun_choices)}',
-                             f'whose {random.choice(noun_choices)} is {tempo_description}',
-                             f'a {tempo_description} music', f'set in a {tempo_description} {random.choice(noun_choices)}']
-
-            noun_choices = ['intensity', 'energy']
-            energy_choices = [f'which is {energy_description}', f'with {energy_description} {random.choice(noun_choices)}',
-                              f'a {energy_description} music',
-                              f'whose {random.choice(noun_choices)} is {energy_description}']
-
+        if not pd.isna(genre):
             noun_choices = ['genre', 'style', 'type', 'category']
-            if len(filtered_tag_list) == 0:
-                filtered_tag_list = [random.choice(tag_list)]  # ensure at least have 1 tag
-            if len(filtered_tag_list) == 1:
-                tag_string = filtered_tag_list[0]
-            else:
-                tag_string = ', '.join(filtered_tag_list[:-1]) + f' and {filtered_tag_list[-1]}'
-            tag_choices = [f'this is a track which is {tag_string}',
-                           f'this song has the {random.choice(noun_choices)} of {tag_string}',
-                           f'the music is {tag_string}', f'the {random.choice(noun_choices)} of the music is {tag_string}']
-
-            phrase_tempo = random.choice(tempo_choices)
-            phrase_energy = random.choice(energy_choices)
+            tag_choices = [f'this song has the {random.choice(noun_choices)} of {genre}',
+                           f'the music is {genre}', f'the {random.choice(noun_choices)} of the music is {genre}']
             phrase_tag = random.choice(tag_choices)
+        else:
+            phrase_tag = None
 
-            text_prompt = []
-            if tempo_description is not None:
-                text_prompt.append(phrase_tempo)
-            if energy_description is not None:
-                text_prompt.append(phrase_energy)
-            if not tag_is_empty:
-                text_prompt.append(phrase_tag)
+        phrase_tempo = random.choice(tempo_choices)
+        phrase_energy = random.choice(energy_choices)
 
+        text_prompt = []
+        if tempo_description is not None:
+            text_prompt.append(phrase_tempo)
+        if energy_description is not None:
+            text_prompt.append(phrase_energy)
+        if phrase_tag is not None:
+            text_prompt.append(phrase_tag)
+
+        if len(text_prompt) > 0:
             random.shuffle(text_prompt)
             music_text_prompt = ', '.join(text_prompt) + '.'
+        else:
+            music_text_prompt = ''
 
         # construct motion text prompt
         motion_description = None
