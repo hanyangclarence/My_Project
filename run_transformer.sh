6404d455ffd01a8b7d7f1dfd5609b9e3a7ccd227
hanyangclarence <mahlerrrr76@gmail.com>
2024-02-18 12:44:37 +0800

    update 15

--- run_transformer.sh
+++ run_transformer.sh
@@ -30,7 +30,7 @@ echo ""
 echo " Run started at:- "
 date
 
-srun python train.py --stage train_caption -b configs/train_caption_exp_15_2.yaml -r training_logs/2024-02-17T21-29-20_exp_15_2/checkpoints/last.ckpt --mm_ckpt training_logs/2024-02-12T03-00-56_exp_13_2/checkpoints/e_21.ckpt
+srun python train.py --stage train_caption -b configs/train_caption_exp_15_2.yaml -r training_logs/2024-02-17T23-41-06_exp_15_2/checkpoints/last.ckpt --mm_ckpt training_logs/2024-02-12T03-00-56_exp_13_2/checkpoints/e_21.ckpt
 
 echo "Run completed at:- "
 date
