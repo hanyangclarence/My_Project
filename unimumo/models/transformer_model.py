86bd7c9d917ed0615ba9fb796bc7e05ee8a36784
hanyangclarence <mahlerrrr76@gmail.com>
2024-02-19 13:42:39 +0800

    update

--- unimumo/models/transformer_model.py
+++ unimumo/models/transformer_model.py
@@ -115,6 +115,8 @@ class MusicMotionTransformer(pl.LightningModule):
         return lm
 
     def init_music_motion_lm_with_pretrained(self, ckpt: str):
+        if not os.path.exists(ckpt):
+            print(f'Warning in instantiating music motion lm! [{ckpt}] does not exist!')
         assert os.path.exists(ckpt), f'The provided path {ckpt} does not exist!'
         # load the music motion lm part of the ckpt
         pretrained_sd = torch.load(ckpt, map_location='cpu')['state_dict']
